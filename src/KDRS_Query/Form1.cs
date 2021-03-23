f3f4b5a4246c31104ec7130ab6dc1079afc004bc
Tore Fadnes <tore.fadnes@ikamr.no>
2021-03-23 12:38:44 +0100

    Updated version

--- src/KDRS_Query/Form1.cs
+++ src/KDRS_Query/Form1.cs
@@ -404,6 +404,6 @@ namespace KDRS_Query
     public static class Globals
     {
         public static readonly String toolName = "KDRS Query";
-        public static readonly String toolVersion = "0.7";
+        public static readonly String toolVersion = "0.7.1";
     }
 }
